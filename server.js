64261a80ae3a94c693f17d84a6193fde3c0101da
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-28 13:03:01 -0300

    feat: server is running

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const app = require('./app')
+const app = require('./src/App')
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
-
--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 
 const express = require('express')
-const roues = require('./routes')
-
 const app = express()
 
 app.use(express.json())
-app.use(routes)
+
+app.get('/', (req, res) => {
+  res.send('Servidor rodando')
+})
 
 module.exports = app
