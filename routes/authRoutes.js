764285e00ac428257ee96e0148798e5908565535
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-29 10:52:16 -0300

    feat: finish Auth Router for connection with Database Mysql

---
+++ routes/authRoutes.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const db = require('../config/db')
+
+const router = express.Router()
+
+router.post("/register", async (req, res) => {
+  try {
+    const { user_email, user_name, user_password, user_phone } = req.body
+
+    if (!user_email || !user_name || !user_password || !user_phone) {
+      return res.status(400).json({ error: 'Dados incompletos' })
+    }
+
+    const hashedPassword = await bcrypt.hash(user_password, 10)
+
+    const [result] = await db.execute(
+      "INSERT INTO users (user_email, user_password, user_name, user_phone) VALUES (?, ?, ?, ?)",
+      [user_email, hashedPassword, user_name, user_phone]
+    )
+
+    res.status(201).json({
+      message: 'Usuário registrado com sucesso',
+      user_id: result.insertId
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Erro no registro' })
+  }
+})
+
+module.exports = router
--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 // app.js
 require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const userRoutes = require('../routes/userRoutes')
+const authRoutes = require('../routes/authRoutes')
 
 app.use(express.json())
+
 app.use('/users', userRoutes)
+app.use('/register', authRoutes)
 
 module.exports = app
