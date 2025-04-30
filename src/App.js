ab3b3a6e966a4e7cf224285b0d5cf4a49f669fa8
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-30 12:57:31 -0300

    feat: implementing permission only for my frontend

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ const express = require('express')
 const app = express()
 const userRoutes = require('../routes/userRoutes')
 const authRoutes = require('../routes/authRoutes')
+const cors = require('cors')
 
+app.use(cors({
+    origin: 'http://localhost:3000'
+  }));
+  
 app.use(express.json())
 
 app.use('/', userRoutes)
