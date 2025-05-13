b08f58ceedb7cc3fbbb125144d460aefdaa9834f
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-05-13 14:15:25 -0300

    feat: add import new routes

--- src/App.js
+++ src/App.js
@@ -11,10 +11,14 @@ const userRoutes = require('../routes/userRoutes');
 const authRoutes = require('../routes/authRoutes');
 const jobRoutes = require('../routes/jobRouter');
 const hospitalRoutes = require('../routes/hospitalRoutes');
+const LeaderboardRoutes = require('../routes/LeaderboardRoutes')
+const StaticsRoutes = require('../routes/StatisticsRoutes')
 
 app.use('/', userRoutes);
 app.use('/jobs', jobRoutes);
 app.use('/auth', authRoutes);
 app.use('/hospital', hospitalRoutes);
+app.use('/stats', StaticsRoutes)
+app.use('/leaderboard', LeaderboardRoutes)
 
 module.exports = app;
