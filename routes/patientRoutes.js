d2e9f887a8d8fb954e17a7938b6bbfa4206484ad
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-05-15 13:07:49 -0300

    feat: fix error

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -3,26 +3,25 @@ const jwt = require('jsonwebtoken');
 const db = require('../config/db');
 const { verifyToken } = require('../middlewares/auth');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        const [rows] = await db.query('SELECT * FROM patients ')
-        res.json(rows)
+        const [rows] = await db.query('SELECT * FROM patients');
+        res.json(rows);
     } catch (error) {
-        console.error(error)
-        res.status(500).json({error: 'Erro ao listar Pacientes'})
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao listar Pacientes' });
     }
-})
+});
 
 router.post('/', async (req, res) => {
-
-})
+});
 
 router.delete('/:id', async (req, res) => {
+});
 
-})
+router.put('/:id', async (req, res) => {
+});
 
-router.put('/:id', async (req, res) =>{
-
-})
+module.exports = router;
